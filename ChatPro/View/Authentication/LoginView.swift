77e939e6584973d8d9eabba4fc65b0101fe5049f
drkast <imdawar090@gmail.com>
2024-09-21 08:04:33 +0500

    Login screen text inputs added

--- ChatPro/View/Authentication/LoginView.swift
+++ ChatPro/View/Authentication/LoginView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct LoginView: View {
+    @State private var email=""
+    @State private var password=""
+    
+   
     var body: some View {
         NavigationView {
             VStack(alignment:.leading){
@@ -15,6 +19,17 @@ struct LoginView: View {
                 Text("Hello.").font(.largeTitle).bold()
                 Text("Welcome Back!").font(.largeTitle).bold()
                     .foregroundColor(.blue)
+                
+                VStack(spacing:20){
+                    TextField("Email",text: $email).padding(5)
+                    SecureField("Password",text: $password).padding(5)
+            
+                    Button("title") {
+                        print("heeloo")
+                    }
+
+                }
+                .padding(.vertical,32)
                     Spacer()
             }
             .padding(.leading)
