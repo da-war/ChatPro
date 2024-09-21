524700c591721e1e639294af33019eb84fdf4a01
drkast <imdawar090@gmail.com>
2024-09-21 07:12:10 +0500

    Initial Commit

---
+++ ChatPro/ChatProApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChatProApp.swift
+//  ChatPro
+//
+//  Created by Rana Dawar Abdullah on 21/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct ChatProApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
